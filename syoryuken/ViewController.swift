79e595b2873ed3fa7459df7c99308d8414af2dd3
Y-Ito-55 <fortunekiss4u@icloud.com>
2023-08-31 11:06:34 +0900

    Initial Commit

---
+++ syoryuken/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  syoryuken
+//
+//  Created by Yumi Ito on 2023/08/28.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var animateButton: UIButton!
+    
+    var images: [UIImage] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        for i in 1...20 {
+            if let image = UIImage(named: "attack\(i)"){
+                images.append(image)
+            }
+        }
+        imageView.image = images.first
+    }
+    
+    @IBAction func animateImages(_ sender: UIButton) {
+        imageView.animationImages = images
+        imageView.animationDuration = 2.5
+        imageView.animationRepeatCount = 1
+        imageView.startAnimating()
+        
+    }
+}
+
